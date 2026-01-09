8c5c9a8a32258d76f668251a50d888ff14d79b68
ohotuowo-morgan <125ohotumorgan@gmail.com>
2026-01-09 17:09:21 +0100

    code-along-practice

---
+++ scripts/navigation.js
@@ -0,0 +1,11 @@
+// Store selected items we are goinng to use
+const navbutton = document.querySelector("#ham-btn");
+
+const navlinks = document.querySelector("#nav-bar");
+
+
+// Toggle the show class off and on
+navbutton.addEventListener("click", () => {
+    navbutton.classList.toggle("show");
+    navlinks.classList.toggle("show");
+});
